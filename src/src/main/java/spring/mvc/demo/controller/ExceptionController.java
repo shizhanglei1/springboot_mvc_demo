4b5061ae02e7f270eef6574ccbe7fcfe3553fab4
haha <shizhanglei@tcl.com>
2020-03-26 20:30:40 +0800

    exception

--- src/src/main/java/spring/mvc/demo/controller/ExceptionController.java
+++ src/src/main/java/spring/mvc/demo/controller/ExceptionController.java
@@ -10,16 +10,23 @@ public class ExceptionController {
 
     @RequestMapping("/er1")
     public String exception1(){
-        return "拦截器测试1";
+        return "error/error";
     }
     @RequestMapping("/er2")
     public String exception2(){
         int i = 1/0;
         return "拦截器测试2";
     }
+
+    /**
+     * 在异常拦截器中传入全局变量，全局变量的传送
+     * @param modelMap
+     * @return
+     */
     @RequestMapping("mt")
     public Object modelTest(ModelMap modelMap){
         Object object = modelMap.get("username");
-        return object;
+        System.out.println(object);
+        return "error/error";
     }
 }
